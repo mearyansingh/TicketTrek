0ee32bf6f2ce8239bf0fdbf5f4db3b69aaba9340
Aryan Singh <aryansingh143001@gmail.com>
2024-02-16 16:03:19 +0530

    final done

--- Backend/Server.js
+++ Backend/Server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, '/frontend/build')))
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function (err) {
+		res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'), function (err) {
 			if (err) {
 				res.status(500).send(err)
 			}
